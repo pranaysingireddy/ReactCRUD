ef75144bf3440676b668ef2e3b481b79e8b1b942
pranaysingireddy <psc6f@mail.umkc.edu>
2019-01-05 19:27:30 -0500

    Added CRUD endpoints for services

--- VehicleFactory.Repo/VehicleRepo.cs
+++ VehicleFactory.Repo/VehicleRepo.cs
@@ -1,22 +1,119 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Web;
 using VehicleFactory.Domain;
 
 namespace VehicleFactory.Repo
 {
     public class VehicleRepo
     {
-        //  private string jsonFile = @"C:\Users\prana\Source\repos\VehicleFactory\VehicleFactory.Domain\Vehicles.json";
-
-        private readonly string _jsonstr = File.ReadAllText(@"C:\Users\prana\Source\repos\VehicleFactory\VehicleFactory.Domain\Vehicles.json");
+        private readonly string _filePath = $"{HttpContext.Current.Server.MapPath("~")}/Vehicles.Json";
+        public List<string> Errors = new List<string>();
 
         public List<Vehicle> GetAllVehicles()
         {
-            var listtt = JsonConvert.DeserializeObject<List<Vehicle>>(_jsonstr);
+            var fileData = File.ReadAllText(_filePath);
 
-            return listtt;
+            var result = JsonConvert.DeserializeObject<List<Vehicle>>(fileData);
 
+            return result;
+        }
+
+        public Vehicle GetVehicleById(int vehicleId)
+        {
+            return GetAllVehicles().FirstOrDefault(a => a.VehicleId == vehicleId);
+        }
+
+        public bool SaveVehicle(Vehicle vehicle)
+        {
+            var jsonStr = JObject.FromObject(vehicle);
+            try
+            {
+                var allvehicles = GetAllVehicles();
+                if (!allvehicles.Where(a => a.VehicleId == vehicle.VehicleId).Any())
+                {
+                    allvehicles.Add(vehicle);
+                    string newJsonResult = JsonConvert.SerializeObject(allvehicles, Formatting.Indented);
+                    File.WriteAllText(_filePath, newJsonResult);
+                    return true;
+                }
+                else
+                {
+                    Errors.Add("Another vehicle exists with the same Id");
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Errors.Add("Error Occured");
+                return false;
+            }
+
+        }
+
+
+        public bool UpdateVehicle(Vehicle vehicle)
+        {
+            var jsonStr = JObject.FromObject(vehicle);
+
+            try
+            {
+                var allvehicles = GetAllVehicles();
+                var vehicleToUpdate = allvehicles.FirstOrDefault(a => a.VehicleId == vehicle.VehicleId);
+                if (vehicleToUpdate != null)
+                {
+                    allvehicles[allvehicles.FindIndex(a => a.VehicleId == vehicle.VehicleId)] = vehicle;
+
+                    string newJsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(allvehicles, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(_filePath, newJsonResult);
+                    return true;
+                }
+                else
+                {
+                    Errors.Add("Vehicle is not found to update");
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Errors.Add("Error Occured");
+                return false;
+            }
+
+        }
+
+
+        public bool DeleteVehicle(int vehicleId)
+        {
+            try
+            {
+                var allvehicles = GetAllVehicles();
+                var vehicleToDelete = allvehicles.FirstOrDefault(a => a.VehicleId == vehicleId);
+                if (vehicleToDelete != null)
+                {
+                    allvehicles.Remove(vehicleToDelete);
+                    string newJsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(allvehicles, Newtonsoft.Json.Formatting.Indented);
+                    File.WriteAllText(_filePath, newJsonResult);
+                    return true;
+                }
+                else
+                {
+                    Errors.Add("Vehicle is not found to delete");
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Errors.Add("Error Occured");
+                return false;
+            }
 
         }
     }
--- VehicleFactory.Services/Controllers/VehiclesController.cs
+++ VehicleFactory.Services/Controllers/VehiclesController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
 using System.Web.Http.Cors;
+using VehicleFactory.Domain;
 using VehicleFactory.Repo;
 
 namespace VehicleFactory.Services.Controllers
@@ -13,10 +15,48 @@ namespace VehicleFactory.Services.Controllers
         {
             _repo = new VehicleRepo();
         }
-
+        [HttpGet]
         public IHttpActionResult Get()
         {
             return Ok(_repo.GetAllVehicles());
+        }
+
+        [HttpGet]
+        [Route("api/vehicles/{vehicleId}")]
+        public IHttpActionResult Get(int vehicleId)
+        {
+            return Ok(_repo.GetVehicleById(vehicleId));
+        }
+
+        [HttpPost]
+        public IHttpActionResult Save([FromBody] Vehicle vehicle)
+        {
+            var result = _repo.SaveVehicle(vehicle);
+            return result ? Ok(_repo.GetAllVehicles()) : SendBadRequest();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update([FromBody] Vehicle vehicle)
+        {
+            var result = _repo.UpdateVehicle(vehicle);
+            return result ? Ok(_repo.GetAllVehicles()) : SendBadRequest();
+        }
+
+        [HttpDelete]
+        [Route("api/vehicles/{vehicleId}")]
+        public IHttpActionResult Delete(int vehicleId)
+        {
+            var result = _repo.DeleteVehicle(vehicleId);
+            return result ? Ok(_repo.GetAllVehicles()) : SendBadRequest();
+        }
+
+        private IHttpActionResult SendBadRequest()
+        {
+            foreach (var error in _repo.Errors.Distinct())
+            {
+                ModelState.AddModelError("Errors", error);
+            }
+            return BadRequest(ModelState);
         }
     }
 }
